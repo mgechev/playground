1a7e5223ff02eed11d1940891c96a620ed3f258c
mgechev <mgechev@gmail.com>
2014-05-09 15:11:46 -0700

    Add more tests

--- go/step1/src/fibi/fibi_test.go
+++ go/step1/src/fibi/fibi_test.go
@@ -3,6 +3,10 @@ import "testing"
 
 func TestFibi(t *testing.T) {
   assert(0, 0, t);
+  assert(-1, 0, t);
+  assert(1, 1, t);
+  assert(2, 1, t);
+  assert(5, 5, t);
 }
 
 func assert(arg int, expected int, t *testing.T) {
