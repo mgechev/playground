b0dac2a7f0c37d3c88ca60951b13ba6da7887d40
mgechev <mgechev@gmail.com>
2014-05-08 17:03:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains my initial experience with different programming languages.
---
+++ go/step1/fib.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func fib(n int) int {
+  if n <= 0 {
+    return 0
+  }
+  if n == 1 {
+    return 1
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+
+func main() {
+  fmt.Println(fib(5))
+}
