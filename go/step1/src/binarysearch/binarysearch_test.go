2e7e89d72be966083c574648fe48e8150721b73c
mgechev <mgechev@gmail.com>
2014-05-10 23:29:13 -0700

    Add tests

---
+++ go/step1/src/binarysearch/binarysearch_test.go
@@ -0,0 +1,19 @@
+package main
+import "testing"
+
+func TestBinarySearch(t *testing.T) {
+  assert([]int{}, 1, -1, t)
+  assert([]int{-1}, -1, 0, t)
+  assert([]int{-1, 1}, 1, 1, t)
+  assert([]int{-1, 1, 2, 3}, 3, 3, t)
+  assert([]int{-1, 1, 2, 3}, 4, -1, t)
+  assert([]int{-1, 1, 2, 3}, -4, -1, t)
+  assert([]int{-1, 1, 2, 3, 7}, -4, -1, t)
+  assert([]int{-1, 1, 2, 3, 7}, 9, -1, t)
+}
+
+func assert(array []int, target int, expected int, t *testing.T) {
+  if x := BinarySearch(array, target); x != expected {
+    t.Errorf("BinarySearch(array, %v) = %x not %v", target, expected)
+  }
+}
