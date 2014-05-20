365eec6d7f7dc73f049845c0fd2f591e14ef6750
mgechev <mgechev@gmail.com>
2014-05-19 21:01:25 -0700

    Add distance functionality

--- go/step3/src/struct.go
+++ go/step3/src/struct.go
@@ -1,12 +1,21 @@
 package main
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
 
 type Vector struct {
-  X int
-  Y int
+  X float64
+  Y float64
+}
+
+func (v1 Vector) distance(v2 Vector) float64 {
+  return math.Sqrt(math.Pow(v2.X - v1.X, 2) + math.Pow(v2.Y - v1.Y, 2))
 }
 
 func main() {
   v := Vector{ 1, 2 }
+  v2 := Vector{ 2, 2 }
   fmt.Println(v.X)
+  fmt.Println(v.distance(v2))
 }
