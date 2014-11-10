7ef31d32ad87e0c241e7068746f2b9910cf60be0
mgechev <mgechev@gmail.com>
2014-11-10 15:46:30 +0200

    Validate according to my jshint config

--- algorithms/PathGameDiv2.js
+++ algorithms/PathGameDiv2.js
@@ -1,31 +1,39 @@
-// TopCoder http://community.topcoder.com/stat?c=problem_statement&pm=13506&rd=16080
+/* global console */
+// TopCoder http://community.topcoder.com/
+//    stat?c=problem_statement&pm=13506&rd=16080
 
 var Colors = {
   WHITE: '.',
   BLACK: '#'
 };
 
+function setCell(board, i, j, color) {
+  'use strict';
+  var temp = board[i].split('');
+  temp[j] = color;
+  board[i] = temp.join('');
+}
+
 function hasPath(board) {
+  'use strict';
   function hasPathHelper(board, i, j) {
-    if (i < 0 || i > 1 || j < 0 || j >= board[0].length || board[i][j] !== Colors.WHITE) {
+    if (i < 0 || i > 1 || j < 0 || j >= board[0].length
+      || board[i][j] !== Colors.WHITE) {
       return false;
     }
     if (board[i][j] === Colors.WHITE && board[0].length - 1 === j) {
       return true;
     }
     setCell(board, i, j, Colors.BLACK);
-    return hasPathHelper(board, i + 1, j) || hasPathHelper(board, i, j + 1) || hasPathHelper(board, i - 1, j);
+    return hasPathHelper(board, i + 1, j) ||
+    hasPathHelper(board, i, j + 1) || hasPathHelper(board, i - 1, j);
   }
-  return hasPathHelper(board.slice(), 0, 0) || hasPathHelper(board.slice(), 1, 0);
-}
-
-function setCell(board, i, j, color) {
-  var temp = board[i].split('');
-  temp[j] = color;
-  board[i] = temp.join('');
+  return hasPathHelper(board.slice(), 0, 0) ||
+  hasPathHelper(board.slice(), 1, 0);
 }
 
 function getMaxMoves(board, i, j) {
+  'use strict';
   if (board[i][j] !== Colors.WHITE || !hasPath(board)) {
     return 0;
   }
@@ -46,6 +54,7 @@ function getMaxMoves(board, i, j) {
 }
 
 function calc(board) {
+  'use strict';
   var max = -Infinity, current;
   for (var i = 0; i < board.length; i += 1) {
     for (var j = 0; j < board[0].length; j += 1) {
