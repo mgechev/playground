54d6bf68994fb2cc67febfcab99ef19e2f4343c5
mgechev <mgechev@gmail.com>
2015-07-07 11:27:15 +0300

    Update dummy

--- haskell/monads/dummy.hs
+++ haskell/monads/dummy.hs
@@ -11,3 +11,15 @@ addRight x (left, right) = (left, right + x)
 
 (-:) :: a -> (a -> b) -> b
 x -: f = f x
+
+
+addLeft' :: Birds -> Stick -> Maybe Stick
+addLeft' x (left, right)
+  | abs(left + x - right) >= 3    =   Nothing
+  | otherwise                     =   Just (left + x, right)
+
+addRight' :: Birds -> Stick -> Maybe Stick
+addRight' x (left, right)
+  | abs(right + x - left) >= 3    =   Nothing
+  | otherwise                     =   Just (left, right + x)
+
