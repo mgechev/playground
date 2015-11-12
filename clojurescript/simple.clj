8d7a58a5ccc0eceaecfeff9bee7b19bd2097469e
mgechev <mgechev@gmail.com>
2015-11-12 10:04:35 +0200

    Add a couple of more

--- clojurescript/simple.clj
+++ clojurescript/simple.clj
@@ -1,5 +1,30 @@
 (ns simple-playground)
 
+;; Factorial function
+
+(defn fact [a]
+  (if (<= a 0)
+    1
+    (* a (fact (- a 1)))))
+
+(fact 5)
+
+
+;; Fibonacci function
+
+(defn fib [n]
+  (if (<= n 2)
+    1
+    (+ (fib (- n 1)) (fib (- n 2))))
+  )
+
+(fib 5)
+(fib 10)
+(fib 15)
+
+
+;; Custom flatten function
+
 (defn flatten1 [lst]
     (if (coll? lst)
       (let [s (seq lst)]
@@ -15,6 +40,9 @@
 (flatten1 [1 2 [3] 4])
 (flatten1 [[1 2] [3] 4])
 (flatten1 [[[[1] 2] [3] 4]])
+
+
+;; Quick Sort function
 
 (defn qs [lst]
   (let [[head & tail] lst]
