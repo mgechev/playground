5d459dae61403649d6649eb28dd60c55ba68ea1e
mgechev <mgechev@gmail.com>
2016-02-09 17:47:34 +0200

    Update lexer

--- js/interpreter/lex.js
+++ js/interpreter/lex.js
@@ -21,19 +21,38 @@ export class Lexer {
   constructor(str) {
     this.col = 0;
     this.row = 0;
+    this.current = 0;
     this.str = str;
+    this.strMap = [];
+    let col = 0;
+    let row = 0;
+    for (let i = 0; i < str.length; i += 1) {
+      if (str[i] === '\n') {
+        this.strMap[row] = col - 1;
+        col = 0;
+        row += 1;
+      } else {
+        col += 1;
+      }
+    }
   }
   advance() {
     this.col += 1;
+    this.current += 1;
   }
   back() {
     this.col -= 1;
+    if (this.col < 0) {
+      this.row -= 1;
+      this.col = this.strMap[this.row];
+    }
+    this.current -= 1;
   }
   done() {
-    return this.col >= this.str.length;
+    return this.current >= this.str.length;
   }
   currentChar() {
-    return this.str[this.col];
+    return this.str[this.current];
   }
   isDigit() {
     return /^\d$/.test(this.currentChar());
@@ -89,6 +108,7 @@ export class Lexer {
   lex() {
     let tokens = [];
     while (!this.done()) {
+      let current = this.current;
       let col = this.col;
       let lexeme;
       let token;
@@ -110,6 +130,7 @@ export class Lexer {
         token = new Token(type, lexeme, col, this.row);
       } else if (this.isNewline()) {
         this.row += 1;
+        this.col = 0;
         this.advance();
       } else if (this.isSemicolon()) {
         token = new Token(TOKEN_TYPES.SEMICOLON, ';', col, this.row);
