468029db2e0d1dc2269c5259f0ed04d72c114ec3
mgechev <mgechev@gmail.com>
2018-07-21 21:09:59 -0700

    Bit diff

---
+++ algorithms/sum-bit-difs.js
@@ -0,0 +1,31 @@
+// https://www.geeksforgeeks.org/sum-of-bit-differences-among-all-pairs/
+
+const arr = [1, 3, 5];
+
+const bits = (a, b)=> {
+  a = a.toString(2);
+  b = b.toString(2);
+  const max = Math.max(a.length, b.length);
+  a = a.padStart(max, '0');
+  b = b.padStart(max, '0');
+  let res = 0;
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      res += 1;
+    }
+  }
+  return res;
+};
+
+const sumBitDiffs = a => {
+  let res = 0;
+  for (let i = 0; i < a.length; i += 1) {
+    for (let j = 0; j < a.length; j += 1) {
+      res += bits(a[i], a[j]);
+    }
+  }
+  return res;
+};
+
+console.log(sumBitDiffs(arr));
+
