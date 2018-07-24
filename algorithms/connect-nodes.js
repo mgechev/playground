90683cae57ac4b08a15b4d50cdb5bda1804181ab
mgechev <mgechev@gmail.com>
2018-07-24 12:01:46 -0700

    Connect nodes

---
+++ algorithms/connect-nodes.js
@@ -0,0 +1,58 @@
+// https://www.geeksforgeeks.org/connect-nodes-level-level-order-traversal/
+
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const bst = new Node(
+  6,
+  new Node(
+    3,
+    new Node(
+      1
+    ),
+    new Node(
+      4
+    )
+  ),
+  new Node(
+    8,
+    new Node(
+      7
+    ),
+    new Node(
+      9
+    )
+  )
+);
+
+
+
+const helper = (n, m, h = 0) => {
+  if (!n) {
+    return;
+  }
+  m[h] = m[h] || [];
+  m[h].push(n);
+  helper(n.left, m, h + 1);
+  helper(n.right, m, h + 1);
+};
+
+const connect = n => {
+  const lst = [];
+  helper(n, lst);
+  lst.forEach(l => {
+    for (let i = 1; i < l.length; i += 1)
+      l[i - 1].next = l[i];
+    l[l.length - 1].next = null;
+  });
+};
+
+connect(bst);
+
+console.log(bst);
+
