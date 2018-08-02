20318a01decfecf33d3274cc2ba36a7eb5a6e154
mgechev <mgechev@gmail.com>
2018-08-02 15:44:34 -0700

    Solve two more

---
+++ algorithms/products.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/product-of-array-except-self/description/
+
+const productExceptSelf = a => {
+  let left = [a[0]];
+  let right = [];
+  right[a.length - 1] = a[a.length - 1];
+  for (let i = 1; i < a.length; i += 1) {
+    left[i] = left[i - 1] * a[i];
+  }
+  for (let i = a.length - 2; i >= 0; i -= 1) {
+    right[i] = right[i + 1] * a[i];
+  }
+  const out = [right[1]];
+  for (let i = 1; i < a.length - 1; i += 1) {
+    out[i] = left[i - 1] * right[i + 1];
+  }
+  out[a.length - 1] = left[a.length - 2];
+  return out;
+};
+
+console.log(productExceptSelf([1,2,3,4]));
+
---
+++ algorithms/reverse-words.js
@@ -0,0 +1,5 @@
+const reverseWords = s => s.trim().split(' ').map(s => s.trim()).filter(s => !/^\s+$/.test(s) && s.length).reverse().join(' ');
+
+console.log(reverseWords('the sky is blue'));
+console.log(reverseWords('   a   b '));
+
