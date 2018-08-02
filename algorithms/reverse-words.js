dc625b998baacd7a580d7461c8b9386738e8bb89
mgechev <mgechev@gmail.com>
2018-08-02 15:49:40 -0700

    Add comment

--- algorithms/reverse-words.js
+++ algorithms/reverse-words.js
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/reverse-words-in-a-string/description/
+
 const reverseWords = s => s.trim().split(' ').map(s => s.trim()).filter(s => !/^\s+$/.test(s) && s.length).reverse().join(' ');
 
 console.log(reverseWords('the sky is blue'));
