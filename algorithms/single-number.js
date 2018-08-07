7f4ed114786c296c5b68939dd4d9004c9107eb4a
mgechev <mgechev@gmail.com>
2018-08-06 19:20:17 -0700

    Update with more efficient solution

--- algorithms/single-number.js
+++ algorithms/single-number.js
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/single-number-ii/description/
 
-const singleNumber = nums => {
+const singleNumber2 = nums => {
   const count = {};
   for (let i = 0; i < nums.length; i += 1) {
     count[nums[i]] = count[nums[i]] || 0;
@@ -12,6 +12,23 @@ const singleNumber = nums => {
     }
   }
   return -1;
+};
+
+const singleNumber = nums => {
+  let res = 0;
+  for (let i = 0; i < 32; i += 1) {
+    let sum = 0;
+    for (let j = 0; j < nums.length; j += 1) {
+      if ((nums[j] >>> i) & 1 === 1) {
+        sum++;
+        sum %= 3;
+      }
+    }
+    if (sum !== 0) {
+      res |= sum << i;
+    }
+  }
+  return res;
 };
 
 console.log(singleNumber([0, 1, 0, 1, 0, 1, 99]));
