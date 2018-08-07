1e5fdd540350351e5a97a74bf2c7e8ecccc7154d
mgechev <mgechev@gmail.com>
2018-08-06 19:37:39 -0700

    Update merge intervals

--- algorithms/merge-intervals.js
+++ algorithms/merge-intervals.js
@@ -1,13 +1,13 @@
 // https://leetcode.com/problems/merge-intervals/description/
 
 const merge = intr => {
-  intr.sort((a, b) => a[0] - b[0]);
+  intr = intr.sort((a, b) => a.start - b.start);
   for (let i = 0; i < intr.length - 1; i += 1) {
     const f = intr[i];
     const s = intr[i + 1];
-    if ((f[1] >= s[0] && f[1] <= s[1]) ||
-        (s[1] >= f[0] && s[1] <= f[1])) {
-      const n = [f[0], Math.max(f[1], s[1])];
+    if ((f.end >= s.start && f.end <= s.end) ||
+        (s.end >= f.start && s.end <= f.end)) {
+      const n = [f.start, Math.max(f.end, s.end)];
       intr[i + 1] = n;
       intr.splice(i, 1);
     }
@@ -15,6 +15,7 @@ const merge = intr => {
   return intr;
 };
 
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
 console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
 console.log(merge([[1,4],[4,5]]));
 
