2c6a6b490aaf9d1d119e7d62efbdc97b8a222b32
mgechev <mgechev@gmail.com>
2018-08-09 13:28:16 -0700

    Update with custom solution

--- algorithms/perfect-squares.js
+++ algorithms/perfect-squares.js
@@ -14,8 +14,26 @@ const numSquares = n => {
   return c[n];
 };
 
+const numSquares2 = (n, c = {}) => {
+  if (c[n] !== undefined) return c[n];
+  if (n === 1) return 1;
+  if (n < 0) return Infinity;
+  if (n === 0) return 0;
+  let min = Infinity;
+  for (let i = 1; i * i <= n; i += 1) {
+    min = Math.min(1 + numSquares2(n - (i * i), c), min);
+  }
+  c[n] = min;
+  return min;
+};
+
 console.log(numSquares(32));
 console.log(numSquares(1));
 console.log(numSquares(2));
 console.log(numSquares(46));
+
+console.log(numSquares2(32));
+console.log(numSquares2(1));
+console.log(numSquares2(2));
+console.log(numSquares2(46));
 
