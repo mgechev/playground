cc3c96d7c55025468b80fb3c9a84b38d6be04af3
mgechev <mgechev@gmail.com>
2018-08-19 15:10:10 -0700

    Add two more

--- algorithms/max-profit.js
+++ algorithms/max-profit.js
@@ -1,28 +1,47 @@
-// https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-k-times/
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 
-const maximizeProfit = (arr, k, idx = 0, memo = {}) => {
-  memo[idx] = memo[idx] || [];
-  if (memo[idx][k] !== undefined) {
-    return memo[idx][k];
-  }
-  if (!k) return 0;
-  if (idx >= arr.length) {
-    return 0;
-  }
-  let total = 0;
-  for (let i = idx; i < arr.length - 1; i += 1) {
-    let sum = 0;
-    for (let j = i + 1; j < arr.length; j += 1) {
-      sum = (arr[j] - arr[i]) + maximizeProfit(arr, k - 1, j + 1, memo);
-      total = Math.max(total, sum);
+const maxProfit2 = s => {
+  let max = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    for (let j = i + 1; j < s.length; j++) {
+      max = Math.max(max, s[j] - s[i]);
     }
   }
-  memo[idx][k] = total;
-  return total;
+  return max;
 };
 
-console.log(maximizeProfit([10, 22, 5, 75, 65, 80], 2));
-console.log(maximizeProfit([12, 14, 17, 10, 14, 13, 12, 15], 3));
-console.log(maximizeProfit([100, 30, 15, 10, 8, 25, 80], 3));
-console.log(maximizeProfit([90, 80, 70, 60, 50], 1));
+const maxProfit3 = s => {
+  let min = 0;
+  let max = 0;
+  let res = s[max] - s[min];
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] > s[max] || min >= max) {
+      max = i;
+    }
+    if (s[i] < s[min]) {
+      min = i;
+    }
+    if (max > min) {
+      res = Math.max(res, s[max] - s[min]);
+    }
+  }
+  return res;
+};
+
+const maxProfit = s => {
+  let min = s[0];
+  let res = 0;
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] < min) {
+      min = s[i];
+    }
+    if (s[i] - min > res) {
+      res = s[i] - min;
+    }
+  }
+  return res;
+};
+
+console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([1,6,7,0,7,4]));
 
