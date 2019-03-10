1263ae971b1e3441f97102b990e34a6fd749b6c8
Minko Gechev <mgechev@google.com>
2019-03-09 21:25:38 -0800

    Simplify implementation

--- ts/cont/monad.ts
+++ ts/cont/monad.ts
@@ -1,24 +1,20 @@
-class Cont<A, R> {
-  constructor(private v: (x: (v: A) => R) => R) {}
+class Cont<A> {
+  constructor(public cont: <R>(x: (v: A) => R) => R) {}
 
-  bind(k: (x: A) => Cont<A, R>): Cont<A, R> {
-    return new Cont(x => this.runCont(a => k(a).runCont(x)));
+  bind(k: (x: A) => Cont<A>): Cont<A> {
+    return new Cont(x => this.cont(a => k(a).cont(x)));
   }
 
-  runCont(f: (v: A) => R) {
-    return this.v(f);
-  }
-
-  static return<A, R>(v: A) {
-    return new Cont<A, R>((x: (v: A) => R) => x(v));
+  static return<A>(v: A) {
+    return new Cont<A>(x => x(v));
   }
 }
 
 const addNum = (a: number, b: number) => a + b;
-const addCPS = (a: number, b: number) => Cont.return<number, void>(addNum(a, b));
+const addCPS = (a: number, b: number) => Cont.return<number>(addNum(a, b));
 
 const squareNum = (a: number) => a * a;
-const squareCPS = (a: number) => Cont.return<number, void>(squareNum(a));
+const squareCPS = (a: number) => Cont.return<number>(squareNum(a));
 
 const pythCPS = (x: number, y: number) => squareCPS(x)
   .bind(xsq => squareCPS(y)
@@ -31,4 +27,4 @@ const pythCPS = (x: number, y: number) => squareCPS(x)
 const a = pythCPS(1, 3)
 
 console.log('Beginning');
-a.runCont(console.log);
+a.cont(console.log);
