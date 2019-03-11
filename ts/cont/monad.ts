a704b15de6f6737d6148faeb377ef569e48246b8
Minko Gechev <mgechev@google.com>
2019-03-10 19:47:49 -0700

    Add examples

--- ts/cont/monad.ts
+++ ts/cont/monad.ts
@@ -10,6 +10,24 @@ class Cont<A> {
   }
 }
 
+// Emulation of exception handling
+
+const exception = () => Cont.return(1)
+  .bind(n => new Cont((n) => 'fred' as any)
+  .bind(res => Cont.return(res as any + n)));
+
+console.log(exception().cont(console.log));
+
+// Sum of two numbers
+
+const sum = () => Cont.return(1)
+  .bind(n1 => Cont.return(2)
+  .bind(n2 => Cont.return(n1 + n2)));
+
+sum().cont(console.log);
+
+// Pythagorean theorem
+
 const addNum = (a: number, b: number) => a + b;
 const addCPS = (a: number, b: number) => Cont.return(addNum(a, b));
 
