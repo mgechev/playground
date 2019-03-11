1327d377c980513f954362b199b2b95a42c44c8e
Minko Gechev <mgechev@google.com>
2019-03-10 20:08:58 -0700

    Add examples

--- ts/cont/monad.ts
+++ ts/cont/monad.ts
@@ -16,7 +16,7 @@ const exception = () => Cont.return(1)
   .bind(n => new Cont((n) => 'fred' as any)
   .bind(res => Cont.return(res as any + n)));
 
-console.log(exception().cont(console.log));
+console.log('Exception:', exception().cont(console.log));
 
 // Sum of two numbers
 
@@ -24,7 +24,31 @@ const sum = () => Cont.return(1)
   .bind(n1 => Cont.return(2)
   .bind(n2 => Cont.return(n1 + n2)));
 
-sum().cont(console.log);
+sum().cont(console.log.bind(console, 'Sum:'));
+
+// Function declaration inside of a monad
+
+const explicit = () => Cont.return(1)
+  .bind(n1 => new Cont<number>(f => f(2))
+  .bind(n2 => Cont.return(n1 + n2)));
+
+explicit().cont(console.log.bind(console, 'Declaration:'));
+
+// Call twice
+
+const callTwice = () => Cont.return(3)
+  .bind(n1 => new Cont<number>(f => f(2) as any + f(3))
+  .bind(n2 => Cont.return(n1 + n2)));
+
+callTwice().cont(console.log.bind(console, 'Twice:'));
+
+// Emulate list
+
+const emulateList = () => Cont.return(1)
+  .bind(n1 => new Cont(f => (f(10) as any).concat(f(11)))
+  .bind(n2 => Cont.return(n1 as any + n2)));
+
+console.log('List:', emulateList().cont(l => [l]));
 
 // Pythagorean theorem
 
