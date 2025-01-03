f0ae3f63e76c13a41266ceefa2e8a054175a8378
mgechev <mgechev@gmail.com>
2025-01-02 17:51:48 -0800

    Add solution for removal of duplicates

---
+++ js/problems/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = nums => {
+    let k = 0;
+    let removed = 0;
+    for (let i = 0; i < nums.length;) {
+        let current = nums[i];
+        let idx = i;
+        while (idx < nums.length - removed && current === nums[idx]) idx++;
+        let reps = idx - i;
+        let finalReps = Math.min(2, reps);
+        removed += reps - finalReps;
+        k += finalReps;
+        let insertionIdx = i + finalReps;
+        for (let j = idx; j < nums.length; j++) {
+            nums[insertionIdx++] = nums[j];
+        }
+        i = i + finalReps;
+        if (i >= nums.length - removed) {
+            break;
+        }
+    }
+    return k;
+};
