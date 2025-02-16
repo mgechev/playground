12d914f485ad0d3f276a0be854978b3969038c2d
Minko Gechev <mgechev@gmail.com>
2025-02-16 12:08:31 -0800

    Add suboptimal solution

---
+++ js/problems/best-time-buy-sell-ii.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = (prices, day = 0, profit = 0, cache = {}) => {
+    if (day >= prices.length) {
+        return profit;
+    }
+    if (cache[day] && cache[day][profit] !== undefined) {
+        return cache[day][profit];
+    }
+    let max = profit;
+    for (let i = day + 1; i < prices.length; i++) {
+        if (prices[i] - prices[day] <= 0) {
+            max = Math.max(max, profit, maxProfit(prices, i, profit, cache));
+        } else {
+            max = Math.max(max, profit, maxProfit(prices, i, profit + prices[i] - prices[day], cache));
+        }
+    }
+    cache[day] = cache[day] || {};
+    cache[day][profit] = max;
+    return max;
+};
+
+
