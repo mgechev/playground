861035ada85de736e9deb00f8ad5fdc9340c8d33
Minko Gechev <mgechev@gmail.com>
2025-02-17 18:36:44 -0800

    Simplify the solution

--- js/problems/jump-game-ii.js
+++ js/problems/jump-game-ii.js
@@ -1,14 +1,14 @@
 // https://leetcode.com/problems/jump-game-ii/
-const jump = (nums, start = 0, total = 0, cache = {}) => {
+const jump = (nums, start = 0, cache = {}) => {
     if (start >= nums.length - 1) {
-        return cache[start] = total;
+        return cache[start] = 0;
     }
     if (cache[start]) {
         return cache[start];
     }
     let result = Infinity;
     for (let i = nums[start]; i > 0; i--) {
-        result = Math.min(1 + jump(nums, start + i, total, cache), result);
+        result = Math.min(1 + jump(nums, start + i, cache), result);
     }
     return cache[start] = result;
 };
