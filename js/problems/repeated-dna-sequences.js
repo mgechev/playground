334640079b374982b916966f0eb1bbc8342b7c19
Minko Gechev <mgechev@gmail.com>
2025-03-01 13:10:09 -0800

    Add optimized naive

--- js/problems/repeated-dna-sequences.js
+++ js/problems/repeated-dna-sequences.js
@@ -22,3 +22,33 @@ const findRepeatedDnaSequences = s => {
     }
     return Array.from(result);
 };
+
+
+// Naive optimized
+const findRepeatedDnaSequences2 = s => {
+    let left = 0;
+    let right = left + 1;
+    let leftBak = 0;
+    let start = -1;
+    const result = new Set();
+    while (left < s.length - 1) {
+        right = left + 1;
+        let foundMatch = false;
+        for (let i = right; i < s.length - 9 && !foundMatch; i++) {
+            if (s[left] === s[i]) {
+                let ri = i;
+                let li = left;
+                while (s[ri] === s[li] && ri - i < 10 && ri < s.length) {
+                    ri++;
+                    li++;
+                }
+                if (ri - i >= 10) {
+                    result.add(s.substring(i, i + 10));
+                    foundMatch = true;
+                }
+            }
+        }
+        left++;
+    }
+    return Array.from(result);
+};
