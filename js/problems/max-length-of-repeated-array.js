81f47a6ff763bfcd60185af255b06890fd7f1299
mgechev <mgechev@gmail.com>
2025-03-04 13:18:32 -0800

    Add iterative dp

--- js/problems/max-length-of-repeated-array.js
+++ js/problems/max-length-of-repeated-array.js
@@ -42,3 +42,23 @@ const lengthOfCommonSubsequence = (a, b, i, j) => {
     while (a[i + length] === b[j + length] && i + length < a.length && j + length < b.length) length++;
     return length;
 };
+
+// Iterative DP
+const findLength = (nums1, nums2) => {
+    const memo = new Array(nums1.length + 1).fill(0).map(() => new Array(nums2.length + 1).fill(0));
+    for (let i = 1; i <= nums1.length; i++) {
+        for (let j = 1; j <= nums2.length; j++) {
+            if (nums1[i - 1] !== nums2[j - 1]) {
+                continue;
+            }
+            memo[i][j] = 1 + memo[i - 1][j - 1];
+        }
+    }
+    let max = 0;
+    for (let i = 0; i < memo.length; i++) {
+        for (let j = 0; j < memo[i].length; j++) {
+            max = Math.max(max, memo[i][j]);
+        }
+    }
+    return max;
+};
